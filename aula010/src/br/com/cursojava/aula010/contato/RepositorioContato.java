b16b1da24eec6af243aae01e96c5287e919d92a0
TrowaBR <raphaelcteodoro@hotmail.com>
2018-12-08 12:57:43 -0200

    JPA

---
+++ aula010/src/br/com/cursojava/aula010/contato/RepositorioContato.java
@@ -0,0 +1,117 @@
+package br.com.cursojava.aula010.contato;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.cursojava.aula010.utils.JPAUtil;
+
+public class RepositorioContato {
+	
+	public List<Contato> buscarTodos() {
+		EntityManager em = JPAUtil.createEntityManager();
+		TypedQuery<Contato> query = em.createQuery("select c from Contato c", Contato.class);
+		List<Contato> lista = query.getResultList();
+		em.close();
+		return lista;
+	}
+	
+	public Contato buscarPorId(Integer id) {
+		EntityManager em = JPAUtil.createEntityManager();
+		Contato c = em.find(Contato.class, id);
+		em.close();
+		return c;
+	}
+
+	public Contato buscarPorTelefone(String telefone) {
+		EntityManager em = JPAUtil.createEntityManager();
+		TypedQuery<Contato> query = em.createQuery("select c from Contato c where c.telefone = :telefone", Contato.class);
+		query.setParameter("telefone", telefone);
+		
+		Contato c;
+		try {
+			c = query.getSingleResult();
+		} catch (Exception e) {
+			c = null;
+		}
+		em.close();
+		return c;
+	}
+
+	public List<Contato> buscarPorNome(String nome) {
+		EntityManager em = JPAUtil.createEntityManager();
+		TypedQuery<Contato> query = em.createQuery("select c from Contato c where upper(c.nome) like upper(:nome)", Contato.class);
+		query.setParameter("nome", "%" + nome + "%");
+		
+		List<Contato> lista = query.getResultList();
+		em.close();
+
+		return lista;
+	}
+	
+	public Contato salvar(Contato contato) {
+		if (contato != null) {
+			if (contato.getId() == null) {
+				return inserir(contato);
+			} else {
+				return atualizar(contato);
+			}
+		}
+		return contato;
+	}
+
+	private Contato atualizar(Contato contato) {
+		EntityManager em = JPAUtil.createEntityManager();
+		em.getTransaction().begin();
+		contato = em.merge(contato);
+		em.getTransaction().commit();
+		em.close();
+		return contato;
+	}
+
+	private Contato inserir(Contato contato) {
+		EntityManager em = JPAUtil.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(contato);
+		em.getTransaction().commit();
+		em.close();
+		return contato;
+	}
+	
+	public void remover(Contato contato) {
+		EntityManager em = JPAUtil.createEntityManager();
+		em.getTransaction().begin();
+		em.remove(em.merge(contato));
+		em.getTransaction().commit();
+		em.close();
+	}
+
+//	public static void main(String[] args) {
+//		EntityManager em = JPAUtil.createEntityManager();
+//
+////		Contato c = new Contato();
+////		c.setNome("Bagrinho");
+////		c.setEmail("b@teste.com");
+////		c.setNome("999887700");
+////		em.getTransaction().begin();
+////		em.persist(c);
+////		em.getTransaction().commit();
+//		
+////		Contato c = em.find(Contato.class, 1);
+////		c.setNome("Bagrinho da Silva");
+////		em.getTransaction().begin();
+////		em.getTransaction().commit();
+//
+//		Contato c = em.find(Contato.class, 1);
+//		c.setNome("Bagrinho da Silva");
+//		em.close();
+//		em = JPAUtil.createEntityManager();
+//		contato = em.merge(contato);
+//		em.getTransaction().begin();
+//		em.getTransaction().commit();
+//
+//		em.close();
+//		JPAUtil.closeEmf();
+//	}
+}
---
+++ aula010/src/br/com/cursojava/aula010/utils/JPAUtil.java
@@ -0,0 +1,31 @@
+package br.com.cursojava.aula010.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	
+	private static EntityManagerFactory emf;
+	
+	private static boolean hasEmf() {
+		return (emf != null && emf.isOpen());
+	}
+
+	public static EntityManagerFactory getEmf() {
+		if (!hasEmf()) {
+			emf = Persistence.createEntityManagerFactory("aula010");
+		}
+		return emf;
+	}
+	
+	public static EntityManager createEntityManager() {
+		return getEmf().createEntityManager();
+	}
+	
+	public static void closeEmf() {
+		if (hasEmf()) {
+			emf.close();
+		}
+	}
+}
